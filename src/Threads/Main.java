b06c826a6dd9006caf2f78c6169425de4a807cc1
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-11-13 13:45:18 +0100

    rename nt1 and nt2 to mt1 and mt2

--- src/Threads/Main.java
+++ src/Threads/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyThread nt1 = new MyThread(20000);
-        MyThread nt2 = new MyThread();
+        MyThread mt1 = new MyThread(20000);
+        MyThread mt2 = new MyThread();
 
         Thread rt1;
         Thread rt2;
@@ -20,35 +20,35 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
 
-            if(command.equals("1") && !nt1.isRunning()) {
-                rt1 = new Thread(nt1, "MyThread1");
+            if(command.equals("1") && !mt1.isRunning()) {
+                rt1 = new Thread(mt1, "MyThread1");
                 elapsed1 = System.currentTimeMillis();
                 rt1.start();
                 // nt.run();  program is blocked
             }
 
-            else if (command.equals("2") && !nt2.isRunning()) {
-                rt2 = new Thread(nt2, "MyThread2");
+            else if (command.equals("2") && !mt2.isRunning()) {
+                rt2 = new Thread(mt2, "MyThread2");
                 elapsed2 = System.currentTimeMillis();
                 rt2.start();
             }
 
-            else if (command.equals("stop1") && nt1.isRunning()) {
-                nt1 = null;
+            else if (command.equals("stop1") && mt1.isRunning()) {
+                mt1 = null;
                 rt1 = null;
                 float time = System.currentTimeMillis() - elapsed1;
                 System.out.println("Elapsed time 1: " + time);
-                nt1 = new MyThread();
-                rt1 = new Thread(nt1, "MyThread");
+                mt1 = new MyThread();
+                rt1 = new Thread(mt1, "MyThread");
             }
 
-            else if (command.equals("stop2") && nt2.isRunning()) {
-                nt2 = null;
+            else if (command.equals("stop2") && mt2.isRunning()) {
+                mt2 = null;
                 rt2 = null;
                 float time = System.currentTimeMillis() - elapsed2;
                 System.out.println("Elapsed time 2: " + time);
-                nt2 = new MyThread();
-                rt2 = new Thread(nt2, "MyThread2");
+                mt2 = new MyThread();
+                rt2 = new Thread(mt2, "MyThread2");
             }
 
             else if (command.toLowerCase().equals("quit")) {
@@ -57,10 +57,10 @@ public class Main {
             }
 
             else {
-                if (nt1.isRunning()) {
+                if (mt1.isRunning()) {
                     System.out.println("Thread 1 is running");
                 }
-                if (nt2.isRunning()) {
+                if (mt2.isRunning()) {
                     System.out.println("Thread 2 is running");
                 }
             }
