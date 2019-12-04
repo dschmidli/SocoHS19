42a04b9d2dd2c1d6f807cc54ada397ec9d435f6c
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-12-04 16:15:44 +0100

    added testing

---
+++ src/Testing/FirstTestingTest.java
@@ -0,0 +1,13 @@
+package Testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstTestingTest {
+
+    @Test
+    public void tTest() {
+
+    }
+}
